9ef2e6492cb2e7ed75bf04dbddfcc7d90c632c45
Moritz Hesche <mo.hesche@gmail.com>
2023-02-12 12:39:04 +0100

    grasp sequence

--- alr_simulation_tools/scene_utils.py
+++ alr_simulation_tools/scene_utils.py
@@ -5,7 +5,7 @@ import numpy as np
 from alr_sim.sims.SimFactory import SimRepository
 from alr_sim.sims.universal_sim.PrimitiveObjects import Box
 from alr_sim.core import Scene
-from alr_sim.sims import MjCamera
+from alr_sim.sims import MjCamera, MujocoCamera
 
 
 def reset_scene(factory_string: str, scene: Scene, agent):
@@ -44,6 +44,7 @@ def create_sample_data(
     object_list: List = None,
     target_obj_name: str = None,
     render_mode: Scene.RenderMode = Scene.RenderMode.BLIND,
+    wait_time: float = 0.1,
 ):
     if object_list is None:
         box1 = Box(
@@ -65,6 +66,9 @@ def create_sample_data(
     # configure camera
     if factory_string == "mj_beta":
         cam = MjCamera("my_cam", init_pos=cam_pos, init_quat=cam_quat)
+    elif factory_string == "mujoco":
+        raise NotImplementedError()
+        # cam = MujocoCamera("my_cam", init_pos=cam_pos, init_quat=cam_quat)
     else:
         raise NotImplementedError()
     scene.add_object(cam)
@@ -75,6 +79,7 @@ def create_sample_data(
 
     # go to start position
     agent.gotoCartPositionAndQuat(robot_pos, robot_quat, duration=0.5)
+    agent.wait(wait_time)
 
     # get camera data
     rgb_img, depth_img = cam.get_image()
@@ -99,9 +104,93 @@ def create_sample_data(
         "rgb_img": rgb_img,
         "depth_img": depth_img,
         "seg_img": seg_img,
+        "seg_img_all": seg_img_orig,
         "point_cloud_seg": point_cloud_seg,
         "point_cloud": point_cloud,
         "cam_pos": cam_pos,
         "cam_quat": cam_quat,
         "cam_intrinsics": cam_intrinsics,
     }
+
+
+def execute_grasping_sequence(
+    agent,
+    home_pos: np.array,
+    grasp_pos: np.array,
+    drop_pos: np.array,
+    grasp_quat: np.array,
+    hover_offset: np.array = (0, 0, 0.05),
+    movement_time: float = 4,
+    grasp_movement_time: float = 2,
+    wait_time: float = 1,
+    logging: bool = False,
+):
+    hover_offset = np.array(hover_offset)
+
+    # assumes that the agent is in a safe position similar to home_pos
+
+    # go to position above grasp position
+    if logging:
+        print(f"Going to position above grasp position {grasp_pos + hover_offset}")
+    agent.gotoCartPositionAndQuat(
+        grasp_pos + hover_offset, grasp_quat, duration=movement_time
+    )
+    agent.wait(wait_time)
+
+    # open gripper
+    if logging:
+        print("Opening gripper")
+    agent.open_fingers()
+    agent.wait(wait_time)
+
+    # go to grasp position
+    if logging:
+        print(f"Going to grasp position {grasp_pos}")
+    agent.gotoCartPositionAndQuat(grasp_pos, grasp_quat, duration=grasp_movement_time)
+    agent.wait(wait_time)
+
+    # close gripper
+    if logging:
+        print("Closing gripper")
+    agent.close_fingers()
+    agent.wait(wait_time)
+
+    # go to position above grasp position
+    if logging:
+        print(f"Going to position above grasp position {grasp_pos + hover_offset}")
+    agent.gotoCartPositionAndQuat(
+        grasp_pos + hover_offset, grasp_quat, duration=grasp_movement_time
+    )
+    agent.wait(wait_time)
+
+    # go to home position
+    if home_pos is not None:
+        if logging:
+            print(f"Going to home position {home_pos}")
+        agent.gotoCartPositionAndQuat(home_pos, grasp_quat, duration=movement_time)
+        agent.wait(wait_time)
+
+    # go to drop position
+    if logging:
+        print(f"Going to drop position {drop_pos}")
+    agent.gotoCartPositionAndQuat(drop_pos, grasp_quat, duration=movement_time)
+    agent.wait(wait_time)
+
+    # open gripper
+    if logging:
+        print("Opening gripper")
+    agent.open_fingers()
+    agent.wait(wait_time)
+
+    # go to home position
+    if home_pos is not None:
+        if logging:
+            print(f"Going to home position {home_pos}")
+        agent.gotoCartPositionAndQuat(home_pos, grasp_quat, duration=movement_time)
+        agent.wait(wait_time)
+
+    # close gripper
+    if logging:
+        print("Closing gripper")
+    agent.close_fingers()
+    agent.wait(wait_time)
