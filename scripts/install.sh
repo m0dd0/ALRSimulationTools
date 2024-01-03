1f3c58ad5612dc319a6beba698181fe98eeb423d
Moritz Hesche <mo.hesche@gmail.com>
2024-01-03 14:31:11 +0000

    Update mujoco installation to version 2.3.7

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,13 +21,13 @@ mamba install -c conda-forge scikit-learn addict pandas plyfile tqdm -y -q
 
 # "Do you wish to install the new Mujoco > 2.1 Support? "
 mamba install -c conda-forge imageio -y -q
-pip install mujoco
+pip install mujoco==2.3.7
 
 # "Do you wish to also install Mujoco 2.1 Support? (Legacy) "
-mamba install -c conda-forge glew patchelf -y -q
-conda env config vars set LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin:/usr/lib/nvidia
-conda env config vars set LD_PRELOAD=$LD_PRELOAD:$CONDA_PREFIX/lib/libGLEW.so
-pip install mujoco-py
+# mamba install -c conda-forge glew patchelf -y -q
+# conda env config vars set LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin:/usr/lib/nvidia
+# conda env config vars set LD_PRELOAD=$LD_PRELOAD:$CONDA_PREFIX/lib/libGLEW.so
+# pip install mujoco-py
 
 pip install open3d
 pip install -e $SIMULATION_FRAMEWORK_PATH
