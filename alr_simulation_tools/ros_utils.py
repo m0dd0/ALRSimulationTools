ea31df04dfd1a036f9dfd1b237fe48032aa0ad2a
Moritz Hesche <mo.hesche@gmail.com>
2024-01-03 14:30:46 +0000

    update rosutils

--- alr_simulation_tools/ros_utils.py
+++ alr_simulation_tools/ros_utils.py
@@ -19,7 +19,17 @@ from alr_sim.utils.point_clouds import rgb_array_to_uint32, rgb_float_to_int
 # from alr_sim.core.Camera import Camera
 # from alr_sim.utils.geometric_transformation import posRotMat2TFMat
 # from alr_sim.utils.point_clouds import rgb_array_to_uint32, rgb_float_to_int
-from grasping_benchmarks_ros.srv import GraspPlannerRequest
+try:
+    from grasping_benchmarks_ros.srv import (
+        GraspPlannerRequest,
+        GraspPlannerResponse,
+        GraspPlanner,
+    )
+except ImportError:
+    print("Could not import grasping_benchmarks_ros")
+    GraspPlannerRequest = None
+    GraspPlannerResponse = None
+    GraspPlanner = None
 
 
 def pos_quat_to_ros_msg(
@@ -108,7 +118,7 @@ def depth_img_to_ros_msg(
 
 
 def rgb_img_to_ros_msg(
-    rgb_img: NpArray["*, *, 3", int], header: Optional[Header] = None
+    rgb_img: NpArray["H,W,3", int], header: Optional[Header] = None
 ) -> Image:
     """
     Convert a RGB image to a ROS Image message
@@ -182,14 +192,14 @@ def cam_intrinsics_to_ros_msg(
 
 
 def create_grasp_planner_request(
-    rgb_img: NpArray[Shape["*, *, 3"], Float],
-    depth_img: NpArray[Shape["*, *"], Float],
-    seg_img: NDArray[Shape["*, *"], Int],
-    pc_points: NDArray[Shape["*, 3"], Float],
-    pc_colors: NDArray[Shape["*, 3"], Int],
-    cam_pos: NDArray[Shape["3"], Float],
-    cam_quat: NDArray[Shape["4"], Float],
-    cam_intrinsics: NDArray[Shape["3, 3"], Float],
+    rgb_img: NpArray["H,W,3", float],
+    depth_img: NpArray["H,W", float],
+    seg_img: NpArray["H,W", int],
+    pc_points: NpArray["N,3", float],
+    pc_colors: NpArray["N,3", int],
+    cam_pos: NpArray["3", float],
+    cam_quat: NpArray["4", float],
+    cam_intrinsics: NpArray["3, 3", float],
     cam_height: float,
     cam_width: float,
     num_of_candidates: int = 1,
@@ -209,6 +219,8 @@ def create_grasp_planner_request(
     :param num_of_candidates: Number of grasp candidates to return
     :return: GraspPlannerRequest ROS message
     """
+    assert GraspPlannerRequest is not None, "Could not import grasping_benchmarks_ros"
+
     planner_req = GraspPlannerRequest()
 
     header = Header()
@@ -228,203 +240,203 @@ def create_grasp_planner_request(
     return planner_req
 
 
-def get_filtered_pc(rgb_img, depth_img, seg_img, seg_obj_id, cam):
-    """
-    Get the PointCloud only of the object of interest
-    If seg_obj_id is None, we assume that the segmentation image is binary
-    :param rgb_img: RGB image in width,height,rgb
-    :param depth_img: Depth image in width,height,meters
-    :param seg_img: Segmentation image in width,height,(true, false)
-    :param seg_obj_id: Segmentation object id
-    :param cam: SimFramework Camera object
-    :return: PointCloud in (x,y,z) (r,g,b)
-    """
-    if seg_obj_id is not None:
-        seg_img = np.where(seg_img == seg_obj_id, 1, 0)
+# def get_filtered_pc(rgb_img, depth_img, seg_img, seg_obj_id, cam):
+#     """
+#     Get the PointCloud only of the object of interest
+#     If seg_obj_id is None, we assume that the segmentation image is binary
+#     :param rgb_img: RGB image in width,height,rgb
+#     :param depth_img: Depth image in width,height,meters
+#     :param seg_img: Segmentation image in width,height,(true, false)
+#     :param seg_obj_id: Segmentation object id
+#     :param cam: SimFramework Camera object
+#     :return: PointCloud in (x,y,z) (r,g,b)
+#     """
+#     if seg_obj_id is not None:
+#         seg_img = np.where(seg_img == seg_obj_id, 1, 0)
 
-    # Segmenting the Images before going to the PointCloud
-    seg_depth_img = depth_img.copy()
-    seg_depth_img[seg_img != 1] = np.nan
-    pc_points, pc_colors = cam.calc_point_cloud_from_images(
-        rgb_img=rgb_img, depth_img=seg_depth_img
-    )
+#     # Segmenting the Images before going to the PointCloud
+#     seg_depth_img = depth_img.copy()
+#     seg_depth_img[seg_img != 1] = np.nan
+#     pc_points, pc_colors = cam.calc_point_cloud_from_images(
+#         rgb_img=rgb_img, depth_img=seg_depth_img
+#     )
 
-    return pc_points, pc_colors
-
-
-def convert_ros_grasp_to_action(reply: GraspPlannerResponse, table_height: float):
-    """
-    Converts the ROS Grasp Planner reply to a grasp action
-    :param reply: ROS Grasp Planner reply
-    :param table_height: Height of the tabletop in world coordinates
-    :return: Grasp Action (Position, Quaternion, Width)
-    """
-    grasps = []
-    for grasp_candidate in reply.grasp_candidates:
-        pose = grasp_candidate.pose.pose
-        grasp = {
-            "position": [pose.position.x, pose.position.y, pose.position.z],
-            "quat": [
-                pose.orientation.w,
-                pose.orientation.x,
-                pose.orientation.y,
-                pose.orientation.z,
-            ],
-            "width": grasp_candidate.width.data,
-            "score": grasp_candidate.score.data,
-        }
-
-        grasp_collides = check_grasp_table_collision(
-            grasp_pos=grasp["position"],
-            grasp_quat=grasp["quat"],
-            width=grasp["width"],
-            table_z=table_height,
-        )
-
-        if not grasp_collides:
-            grasps.append(grasp)
-
-    if len(grasps) == 0:
-        print("There was no grasp without table collision detected")
-        return None, None, None, True
-
-    best_grasp_idx = np.argmax([grasp.get("score") for grasp in grasps])
-    best_grasp = grasps[best_grasp_idx]
-    best_grasp_pos = best_grasp.get("position")
-    best_grasp_quat = best_grasp.get("quat")
-    best_grasp_width = best_grasp.get("width")
-
-    return best_grasp_pos, best_grasp_quat, best_grasp_width, False
+#     return pc_points, pc_colors
 
 
-def call_grasp_planner(
-    cam: Camera,
-    service_id: str,
-    seg_obj_id: Optional[int] = None,
-    num_of_candidates: int = 1,
-    filter_pc: bool = True,
-    visualize: bool = False,
-    table_height: float = -0.02,
-) -> Tuple[NDArray[Shape["3"], Float], NDArray[Shape["4"], Float]]:
-    """
-    Call the grasp planner service and return the grasp poses
-    :param cam: SimFramework Camera object
-    :param service_id: Service id of the grasp planner
-    :param seg_obj_id: Segmentation object id
-    :param num_of_candidates: Number of grasp candidates to return
-    :param filter_pc: Filter the PointCloud to only contain the object of interest (bool)
-    :param visualize: Visualize the PointCloud (bool)
-    :param table_height: Table y position (meters)
-    :return: Grasp poses in (x,y,z) (x,y,z,w), and three images of grasp candidates
-    """
+# def convert_ros_grasp_to_action(reply: GraspPlannerResponse, table_height: float):
+#     """
+#     Converts the ROS Grasp Planner reply to a grasp action
+#     :param reply: ROS Grasp Planner reply
+#     :param table_height: Height of the tabletop in world coordinates
+#     :return: Grasp Action (Position, Quaternion, Width)
+#     """
+#     grasps = []
+#     for grasp_candidate in reply.grasp_candidates:
+#         pose = grasp_candidate.pose.pose
+#         grasp = {
+#             "position": [pose.position.x, pose.position.y, pose.position.z],
+#             "quat": [
+#                 pose.orientation.w,
+#                 pose.orientation.x,
+#                 pose.orientation.y,
+#                 pose.orientation.z,
+#             ],
+#             "width": grasp_candidate.width.data,
+#             "score": grasp_candidate.score.data,
+#         }
 
-    # Get Sensor Data
-    seg_img = cam.get_segmentation(depth=False)
-    rgb_img, depth_img = cam.get_image(depth=True)
-    depth_img[depth_img == 0] = np.nan
-    depth_img[depth_img > 1] = np.nan
+#         grasp_collides = check_grasp_table_collision(
+#             grasp_pos=grasp["position"],
+#             grasp_quat=grasp["quat"],
+#             width=grasp["width"],
+#             table_z=table_height,
+#         )
 
-    # Get Cam Info
-    cam_pos, cam_quat = cam.get_cart_pos_quat()
-    cam_intrinsics = cam.intrinsics
-    cam_height = cam.height
-    cam_width = cam.width
+#         if not grasp_collides:
+#             grasps.append(grasp)
 
-    # Create Filtered PointCloud and transform
-    if filter_pc:
-        pc_points, pc_colors = get_filtered_pc(
-            rgb_img, depth_img, seg_img, seg_obj_id, cam
-        )
-        pc_points = transform_pc(pc_points, cam_pos, cam_quat)
-    else:
-        pc_points, pc_colors = cam.calc_point_cloud_from_images(
-            rgb_img=rgb_img, depth_img=depth_img
-        )
+#     if len(grasps) == 0:
+#         print("There was no grasp without table collision detected")
+#         return None, None, None, True
 
-    # Create the Grasp Request out of that
-    grasp_req = create_grasp_planner_request(
-        rgb_img,
-        depth_img,
-        seg_img,
-        pc_points,
-        pc_colors,
-        cam_pos,
-        cam_quat,
-        cam_intrinsics,
-        cam_height,
-        cam_width,
-        num_of_candidates=num_of_candidates,
-    )
+#     best_grasp_idx = np.argmax([grasp.get("score") for grasp in grasps])
+#     best_grasp = grasps[best_grasp_idx]
+#     best_grasp_pos = best_grasp.get("position")
+#     best_grasp_quat = best_grasp.get("quat")
+#     best_grasp_width = best_grasp.get("width")
 
-    # And call the GraspService
-    rospy.wait_for_service(service_id, timeout=30.0)
-    grasp_planner = rospy.ServiceProxy(service_id, GraspPlanner)
+#     return best_grasp_pos, best_grasp_quat, best_grasp_width, False
 
-    # Convert the ROS Result into a more usable format
-    try:
-        reply: GraspPlannerResponse = grasp_planner(grasp_req)
 
-        grasps = []
-        counter_collision_grasps = 0
-        for grasp_candidate in reply.grasp_candidates:
-            pose = grasp_candidate.pose.pose
-            grasp = {
-                "position": [pose.position.x, pose.position.y, pose.position.z],
-                "quat": [
-                    pose.orientation.w,
-                    pose.orientation.x,
-                    pose.orientation.y,
-                    pose.orientation.z,
-                ],
-                "width": grasp_candidate.width.data,
-                "score": grasp_candidate.score.data,
-            }
+# def call_grasp_planner(
+#     cam: Camera,
+#     service_id: str,
+#     seg_obj_id: Optional[int] = None,
+#     num_of_candidates: int = 1,
+#     filter_pc: bool = True,
+#     visualize: bool = False,
+#     table_height: float = -0.02,
+# ) -> Tuple[NDArray[Shape["3"], Float], NDArray[Shape["4"], Float]]:
+#     """
+#     Call the grasp planner service and return the grasp poses
+#     :param cam: SimFramework Camera object
+#     :param service_id: Service id of the grasp planner
+#     :param seg_obj_id: Segmentation object id
+#     :param num_of_candidates: Number of grasp candidates to return
+#     :param filter_pc: Filter the PointCloud to only contain the object of interest (bool)
+#     :param visualize: Visualize the PointCloud (bool)
+#     :param table_height: Table y position (meters)
+#     :return: Grasp poses in (x,y,z) (x,y,z,w), and three images of grasp candidates
+#     """
 
-            grasp_collides = check_grasp_table_collision(
-                grasp_pos=grasp["position"],
-                grasp_quat=grasp["quat"],
-                width=grasp["width"],
-                table_z=table_height,
-            )
+#     # Get Sensor Data
+#     seg_img = cam.get_segmentation(depth=False)
+#     rgb_img, depth_img = cam.get_image(depth=True)
+#     depth_img[depth_img == 0] = np.nan
+#     depth_img[depth_img > 1] = np.nan
 
-            if not grasp_collides:
-                grasps.append(grasp)
-            else:
-                counter_collision_grasps += 1
+#     # Get Cam Info
+#     cam_pos, cam_quat = cam.get_cart_pos_quat()
+#     cam_intrinsics = cam.intrinsics
+#     cam_height = cam.height
+#     cam_width = cam.width
 
-        if len(grasps) == 0:
-            print("There was no grasp without table collision detected")
-            return None
+#     # Create Filtered PointCloud and transform
+#     if filter_pc:
+#         pc_points, pc_colors = get_filtered_pc(
+#             rgb_img, depth_img, seg_img, seg_obj_id, cam
+#         )
+#         pc_points = transform_pc(pc_points, cam_pos, cam_quat)
+#     else:
+#         pc_points, pc_colors = cam.calc_point_cloud_from_images(
+#             rgb_img=rgb_img, depth_img=depth_img
+#         )
 
-        if counter_collision_grasps > 0:
-            print(
-                f"Found {counter_collision_grasps} grasps with collision from {len(reply.grasp_candidates)}"
-            )
+#     # Create the Grasp Request out of that
+#     grasp_req = create_grasp_planner_request(
+#         rgb_img,
+#         depth_img,
+#         seg_img,
+#         pc_points,
+#         pc_colors,
+#         cam_pos,
+#         cam_quat,
+#         cam_intrinsics,
+#         cam_height,
+#         cam_width,
+#         num_of_candidates=num_of_candidates,
+#     )
 
-    except rospy.ServiceException as e:
-        print("Service {} call failed: {}".format(grasp_planner.resolved_name, e))
-        return None
+#     # And call the GraspService
+#     rospy.wait_for_service(service_id, timeout=30.0)
+#     grasp_planner = rospy.ServiceProxy(service_id, GraspPlanner)
 
-    # Visualization for Logging
-    pc_points, pc_colors = cam.calc_point_cloud_from_images(
-        rgb_img=rgb_img, depth_img=depth_img
-    )
-    world_points = transform_pc(pc_points, cam_pos, cam_quat)
+#     # Convert the ROS Result into a more usable format
+#     try:
+#         reply: GraspPlannerResponse = grasp_planner(grasp_req)
 
-    best_grasp_idx = np.argmax([grasp.get("score") for grasp in grasps])
-    best_grasp = grasps[best_grasp_idx]
-    best_grasp_pos = best_grasp.get("position")
-    best_grasp_quat = best_grasp.get("quat")
-    best_grasp_width = best_grasp.get("width")
+#         grasps = []
+#         counter_collision_grasps = 0
+#         for grasp_candidate in reply.grasp_candidates:
+#             pose = grasp_candidate.pose.pose
+#             grasp = {
+#                 "position": [pose.position.x, pose.position.y, pose.position.z],
+#                 "quat": [
+#                     pose.orientation.w,
+#                     pose.orientation.x,
+#                     pose.orientation.y,
+#                     pose.orientation.z,
+#                 ],
+#                 "width": grasp_candidate.width.data,
+#                 "score": grasp_candidate.score.data,
+#             }
 
-    img_mid, img_left, img_right = render_pointcloud_with_panda_grippers(
-        world_points, pc_colors, best_grasp_pos, best_grasp_quat, best_grasp_width
-    )
+#             grasp_collides = check_grasp_table_collision(
+#                 grasp_pos=grasp["position"],
+#                 grasp_quat=grasp["quat"],
+#                 width=grasp["width"],
+#                 table_z=table_height,
+#             )
 
-    # Visualize the pointcloud in UI
-    if visualize:
-        visualize_pointcloud_with_panda_grippers(
-            world_points, pc_colors, grasps=grasps, chosen_idx=best_grasp_idx
-        )
+#             if not grasp_collides:
+#                 grasps.append(grasp)
+#             else:
+#                 counter_collision_grasps += 1
 
-    return grasps, (img_mid, img_left, img_right)
+#         if len(grasps) == 0:
+#             print("There was no grasp without table collision detected")
+#             return None
+
+#         if counter_collision_grasps > 0:
+#             print(
+#                 f"Found {counter_collision_grasps} grasps with collision from {len(reply.grasp_candidates)}"
+#             )
+
+#     except rospy.ServiceException as e:
+#         print("Service {} call failed: {}".format(grasp_planner.resolved_name, e))
+#         return None
+
+#     # Visualization for Logging
+#     pc_points, pc_colors = cam.calc_point_cloud_from_images(
+#         rgb_img=rgb_img, depth_img=depth_img
+#     )
+#     world_points = transform_pc(pc_points, cam_pos, cam_quat)
+
+#     best_grasp_idx = np.argmax([grasp.get("score") for grasp in grasps])
+#     best_grasp = grasps[best_grasp_idx]
+#     best_grasp_pos = best_grasp.get("position")
+#     best_grasp_quat = best_grasp.get("quat")
+#     best_grasp_width = best_grasp.get("width")
+
+#     img_mid, img_left, img_right = render_pointcloud_with_panda_grippers(
+#         world_points, pc_colors, best_grasp_pos, best_grasp_quat, best_grasp_width
+#     )
+
+#     # Visualize the pointcloud in UI
+#     if visualize:
+#         visualize_pointcloud_with_panda_grippers(
+#             world_points, pc_colors, grasps=grasps, chosen_idx=best_grasp_idx
+#         )
+
+#     return grasps, (img_mid, img_left, img_right)
