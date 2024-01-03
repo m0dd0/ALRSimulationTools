90b10cb529f1a879f49b81f631454d1d1315c954
Moritz Hesche <mo.hesche@gmail.com>
2024-01-03 14:31:22 +0000

    Update ros_numpy dependency to latest version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         "numpy",
         "trimesh",
         "scipy",
-        "ros_numpy",
+        "ros_numpy @ git+https://github.com/m0dd0/ros_numpy@master",  # the original version is outdated and does not support new version of numpy
     ],
     extras_require={"dev": ["black", "pylint", "jupyter", "ipykernel"]},
     include_package_data=True,
